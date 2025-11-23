7a3e2774ccd2c3ce90899ef5b233a51a7f2c12d2
SHAIK RAIYAN <shaikraiyan2005@gmail.com>
2025-11-23 10:59:15 +0530

    feat(revealText): add gsap revealtext

--- src/components/Github.jsx
+++ src/components/Github.jsx
@@ -20,7 +20,6 @@ function Github() {
 
   return (
     <div className="px-4">
-      
       <h2 className="text-2xl">GitHub Activity</h2>
       {total !== null && (
         <span className="text-neutral-400">Total: {total} contributions</span>
@@ -29,13 +28,14 @@ function Github() {
         initial={{ width: 0, opacity: 0.5 }}
         whileInView={{ width: "100%", opacity: 1 }}
         transition={{ type: "tween", duration: 1.5 }}
+        viewport={{ margin: "0px 0px -25% 0px"}}
         className="border-b-2 border-gray-400 pt-3 mb-3"
       />
       <a
         href="https://github.com/SHAIK-RAIYAN"
         target="_blank"
         className="hidden md:block mx-auto">
-        <div className="bg-[rgb(8,10,13)] py-3 px-6 w-full mx-auto text-white rounded-lg flex justify-center items-center border border-dashed border-neutral-600">
+        <div className="bg-[rgb(8,10,13)] py-3 px-6 w-full mx-auto text-white rounded-lg flex justify-center items-center border border-dashed border-neutral-700">
           <GitHubCalendar
             colorScheme="dark"
             username="SHAIK-RAIYAN"
---
+++ src/components/ui/TextRevealHover.jsx
@@ -0,0 +1,58 @@
+import gsap from "gsap";
+import { SplitText } from "gsap/SplitText";
+import { useEffect, useRef } from "react";
+
+gsap.registerPlugin(SplitText);
+
+function TextRevealHover({
+  word,
+  className = "",
+  hoverColor = "text-yellow-400",
+}) {
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    if (!containerRef.current) return;
+
+    let ctx;
+    const initAnimation = () => {
+      ctx = gsap.context(() => {
+        const target = containerRef.current;
+        const split = new SplitText(target.querySelectorAll("span"), {
+          type: "chars",
+          charsClass: "inline-block",
+        });
+
+        const tl = gsap.timeline({
+          paused: true,
+          defaults: { ease: "power3.inOut", duration: 0.4 },
+        });
+
+        tl.to(split.chars, {
+          yPercent: -50,
+          stagger: { amount: 0.3 },
+        });
+
+        target.addEventListener("mouseenter", () => tl.play());
+        target.addEventListener("mouseleave", () => tl.reverse());
+      }, containerRef);
+    };
+
+    document.fonts.ready.then(() => initAnimation());
+
+    return () => ctx && ctx.revert();
+  }, [word]);
+
+  return (
+    <div
+      ref={containerRef}
+      className={`relative overflow-hidden cursor-pointer inline-flex flex-col leading-none ${className}`}>
+      <span>{word}</span>
+      <span className={`hover-text absolute top-full left-0 ${hoverColor}`}>
+        {word}
+      </span>
+    </div>
+  );
+}
+
+export default TextRevealHover;
