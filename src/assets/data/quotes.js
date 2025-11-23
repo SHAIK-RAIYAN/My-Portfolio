88533b8c2894a95084fc6c3e908793a680c7c002
SHAIK RAIYAN <shaikraiyan2005@gmail.com>
2025-11-23 23:32:42 +0530

    feat(quotes): add quotes and display card

---
+++ src/assets/data/quotes.js
@@ -0,0 +1,119 @@
+export const quotes = [
+  {
+    quote: "Wake up to reality! Nothing ever goes as planned in this world.",
+    author: "Madara Uchiha",
+    anime: "Naruto Shippuden",
+  },
+  {
+    quote: "People die if they are killed.",
+    author: "Shiro Emiya",
+    anime: "Fate/stay night",
+  },
+  {
+    quote:
+      "I want you to be happy. I want you to laugh a lot. I don’t know what exactly I’ll be able to do for you, but I’ll always be by your side.",
+    author: "Kagome Higurashi",
+    anime: "InuYasha",
+  },
+  {
+    quote: "If you don’t take risks, you can’t create a future.",
+    author: "Monkey D. Luffy",
+    anime: "One Piece",
+  },
+  {
+    quote: "Simplicity is the easiest path to true beauty.",
+    author: "Seishuu Handa",
+    anime: "Barakamon",
+  },
+  {
+    quote:
+      "Whatever you lose, you'll find it again. But what you throw away you'll never get back.",
+    author: "Kenshin Himura",
+    anime: "Rurouni Kenshin",
+  },
+  {
+    quote:
+      "The world isn’t perfect. But it’s there for us, doing the best it can….that’s what makes it so damn beautiful.",
+    author: "Roy Mustang",
+    anime: "Fullmetal Alchemist",
+  },
+  {
+    quote:
+      "To know sorrow is not terrifying. What is terrifying is to know you can't go back to happiness you could have.",
+    author: "Matsumoto Rangiku",
+    anime: "Bleach",
+  },
+  {
+    quote:
+      "We are all like fireworks: we climb, we shine and always go our separate ways and become further apart. But even when that time comes, let's not disappear like a firework and continue to shine.. forever.",
+    author: "Hitsugaya Toshiro",
+    anime: "Bleach",
+  },
+  {
+    quote:
+      "If you have time to think of a beautiful end, then live beautifully until the end.",
+    author: "Sakata Gintoki",
+    anime: "Gintama",
+  },
+  {
+    quote: "Talk is cheap. Show me the code.",
+    author: "Linus Torvalds",
+  },
+  {
+    quote: "The only way to do great work is to love what you do.",
+    author: "Steve Jobs",
+  },
+  {
+    quote: "First, solve the problem. Then, write the code.",
+    author: "John Johnson",
+  },
+  {
+    quote: "Experience is the name everyone gives to their mistakes.",
+    author: "Oscar Wilde",
+  },
+  {
+    quote: "Simplicity is the soul of efficiency.",
+    author: "Austin Freeman",
+  },
+  {
+    quote: "Code is like humor. When you have to explain it, it’s bad.",
+    author: "Cory House",
+  },
+  {
+    quote: "Fix the cause, not the symptom.",
+    author: "Steve Maguire",
+  },
+  {
+    quote: "Make it work, make it right, make it fast.",
+    author: "Kent Beck",
+  },
+  {
+    quote: "Before software can be reusable it first has to be usable.",
+    author: "Ralph Johnson",
+  },
+  {
+    quote: "It’s not a bug – it’s an undocumented feature.",
+    author: "Anonymous",
+  },
+  {
+    quote: "Java is to JavaScript what car is to Carpet.",
+    author: "Chris Heilmann",
+  },
+  {
+    quote: "Knowledge is power.",
+    author: "Francis Bacon",
+  },
+  {
+    quote: "I have not failed. I've just found 10,000 ways that won't work.",
+    author: "Thomas Edison",
+  },
+  {
+    quote: "Simplicity is the ultimate sophistication.",
+    author: "Leonardo da Vinci",
+  },
+  {
+    quote:
+      "Measurement is the first step that leads to control and eventually to improvement.",
+    author: "H. James Harrington",
+  },
+];
