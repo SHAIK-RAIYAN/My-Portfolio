00df55ad098d03dabfbffbe1f2f69beb773d289c
SHAIK RAIYAN <shaikraiyan2005@gmail.com>
2025-11-23 10:43:42 +0530

    fix(Home): add image shaikraiyan text

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -16,7 +16,7 @@ function Home() {
 
   return (
     <motion.div
-      className="flex flex-col justify-center gap-10 text-white h-screen px-6 md:mt-10 overflow-hidden"
+      className="flex flex-col justify-center gap-10 text-white h-screen md:mt-10 overflow-hidden"
       id="home">
       <motion.div
         initial={{ opacity: 0, x: "-80%" }}
@@ -27,7 +27,7 @@ function Home() {
           whileHover={{ scale: 1.02 }}
           whileTap={{ scale: 0.98 }}
           transition={{ duration: 0.3, type: "tween" }}
-          className="relative w-44 h-44 md:h-60 md:w-60 md:mt-0 overflow-hidden rounded-full border border-neutral-500 mx-auto md:mx-0">
+          className="relative w-44 h-44 md:h-60 md:w-60 md:mt-0 overflow-hidden rounded-full border border-neutral-500 mx-auto md:mx-2">
           <img
             src="/Raiyan.jpg"
             alt="Shaik Raiyan"
@@ -36,18 +36,27 @@ function Home() {
         </motion.div>
       </motion.div>
 
-      <div className=" flex flex-col text-4xl font-semibold gap-3 w-full text-center md:text-left">
+      <div className=" flex flex-col text-4xl font-semibold gap-3 w-full px-5 md:text-left">
         <div className="relative overflow-hidden">
           <motion.div
             initial={{ opacity: 0, y: 75 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ type: "easeInOut", delay: 1, duration: 0.5 }}>
-            <h2 className="text-[#e5e7eb] font-pixel text-4xl lg:text-6xl py-4 relative z-20 font-bold tracking-tight">
-              Hi, I'm Shaik Raiyan{" "}
-              <p className="italic font-extralight text-xl text-neutral-600 font-pixel">
-                I write something for machines and they understand!
-              </p>
+            <h2 className="text-[#e5e7eb] text-4xl lg:text-6xl py-4 relative font-normal z-20 tracking-tight">
+              Hi, I'm{" "}
+              <div className="relative inline-block">
+                <span className="absolute inset-0 font-bold text-4xl lg:text-7xl text-amber-500 blur-sm opacity-70">
+                  Shaik Raiyan
+                </span>
+
+                <span className="text-transparent font-bold text-4xl lg:text-7xl bg-[url(/art.jpg)] bg-cover bg-clip-text">
+                  Shaik Raiyan
+                </span>
+              </div>
             </h2>
+            <p className="italic font-extralight text-xl text-neutral-600 font-pixel border-t border-b border-dashed">
+              I write something for machines and they understand!
+            </p>
           </motion.div>
         </div>
 
@@ -56,13 +65,14 @@ function Home() {
             initial={{ opacity: 0, y: 75 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ type: "easeInOut", delay: 1, duration: 0.5 }}
-            className="flex justify-center md:justify-start gap-2 text-3xl md:text-4xl text-[#868686] md:gap-4 whitespace-normal w-auto max-w-full">
+            className="font-pixel flex justify-start gap-2 text-4xl md:text-5xl text-[#868686] md:gap-4 whitespace-normal w-auto max-w-full">
             <Typewriter
               options={{
                 strings: ["A Full-Stack Developer.", "A Java Programmer."],
                 autoStart: true,
                 loop: true,
                 duration: 0.7,
+                delay: 80,
               }}
             />
           </motion.div>
@@ -74,7 +84,7 @@ function Home() {
             animate={{ opacity: 1, y: 0 }}
             transition={{ type: "easeInOut", duration: 0.5, delay: 1.5 }}
             className="py-5">
-            <p className="text-gray-400 text-sm md:text-lg font-normal font-jakarta w-full">
+            <p className="text-gray-400 text-sm md:text-lg font-normal w-full">
               I’m a curious full-stack developer who loves turning ideas into
               smooth, scalable, and fun digital experiences powered by the MERN
               stack, SQL, and Java—with a growing edge in DSA.
